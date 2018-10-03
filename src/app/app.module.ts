505415065dfa4ac2dd6828290588b92c5fefc36c
Ritesh Tiwari <ritesh.mckvie@gmail.com>
2018-10-03 15:44:14 +0530

    added modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonsModule } from './commons/commons.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
